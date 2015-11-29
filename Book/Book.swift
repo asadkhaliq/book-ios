049df6f0aa6e4f45d8903a1bd665647aa72009ad
sarkar17 <sarkar17@stanford.edu>
2015-11-29 23:10:57 +0100

    added fetch all to books

--- Book/Book.swift
+++ Book/Book.swift
@@ -12,7 +12,7 @@ import CoreData
 
 class Book: NSManagedObject {
     
-    class func addBook(title: String, withISBN isbn: String?, numPages pages: Int, withPriority priority: String, because reason: String, withCategory category: String, writtenBy author: String, withContext context: NSManagedObjectContext) -> Book {
+    class func addBook(title: String, withISBN isbn: String?, numPages pages: Int, withPriority priority: String, because reason: String, withCategory category: String, writtenBy author: String, withContext context: NSManagedObjectContext) -> Book? {
         
         if let book = NSEntityDescription.insertNewObjectForEntityForName("Book", inManagedObjectContext: context) as? Book {
             book.title = title
@@ -24,11 +24,23 @@ class Book: NSManagedObject {
             book.dateStarted = nil
             book.currentPage = nil
             book.dateCompleted = nil
-            book.category = Category.getCategoryFromString(category)
-            book.author = Author.getAuthorFromString(author)
-            
+            book.category = Category.getCategoryFromString(category, context: context)
+            book.author = Author.getAuthorFromString(author, context: context)
+            return book
         }
         
+        return nil
+        
+    }
+    
+    class func fetchAllBooks(context: NSManagedObjectContext) -> [Book] {
+        let request = NSFetchRequest(entityName: "Book")
+        
+        if let books = (try? context.executeFetchRequest(request)) as? [Book] {
+            return books
+        } else {
+            return []
+        }
     }
 
 // Insert code here to add functionality to your managed object subclass
