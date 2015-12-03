ab9c76a95bb0da656abcf569da7a45779c086605
sarkar17 <sarkar17@stanford.edu>
2015-12-03 13:15:59 -0800

    grid and list
    changed the library view to have grid and list options

---
+++ Book/LibrarySwitchViewController.swift
@@ -0,0 +1,84 @@
+//
+//  LibrarySwitchViewController.swift
+//  Book
+//
+//  Created by Aditya Sarkar on 12/3/15.
+//  Copyright © 2015 Asad Khaliq. All rights reserved.
+//
+
+import UIKit
+
+class LibrarySwitchViewController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    weak var currentViewController: UIViewController?
+    
+    override func viewDidLoad() {
+        self.currentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("libraryList1")
+        self.currentViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addChildViewController(self.currentViewController!)
+        self.addSubview(self.currentViewController!.view, toView: self.containerView)
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func switchViewType(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            let newViewController = self.storyboard?.instantiateViewControllerWithIdentifier("libraryList1")
+            newViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+            self.cycleFromViewController(self.currentViewController!, toViewController: newViewController!)
+            self.currentViewController = newViewController
+        } else {
+            let newViewController = self.storyboard?.instantiateViewControllerWithIdentifier("libraryGrid")
+            newViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+            self.cycleFromViewController(self.currentViewController!, toViewController: newViewController!)
+            self.currentViewController = newViewController
+        }
+    }
+    
+    func cycleFromViewController(oldViewController: UIViewController, toViewController newViewController: UIViewController) {
+        oldViewController.willMoveToParentViewController(nil)
+        self.addChildViewController(newViewController)
+        self.addSubview(newViewController.view, toView:self.containerView!)
+        newViewController.view.alpha = 0
+        newViewController.view.layoutIfNeeded()
+        UIView.animateWithDuration(0.1, animations: {
+            newViewController.view.alpha = 1
+            oldViewController.view.alpha = 0
+            },
+            completion: { finished in
+                oldViewController.view.removeFromSuperview()
+                oldViewController.removeFromParentViewController()
+                newViewController.didMoveToParentViewController(self)
+        })
+    }
+    
+    func addSubview(subView:UIView, toView parentView:UIView) {
+        parentView.addSubview(subView)
+        
+        var viewBindingsDict = [String: AnyObject]()
+        viewBindingsDict["subView"] = subView
+        parentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[subView]|",
+            options: [], metrics: nil, views: viewBindingsDict))
+        parentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[subView]|",
+            options: [], metrics: nil, views: viewBindingsDict))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
