bd97cd5fe3a7bccc1daef1e6728c37021524e869
asadkhaliq <Asad@Asads-MacBook-Pro.local>
2015-12-03 08:24:38 -0800

    Many changes to settings page
    Added location view page, can choose current location or search for
    city; added user profile page, can choose image from camera or from
    photo picker (camera only works on actual devices); added media player
    page (currently empty); put shit into navigation bars

---
+++ Book/SettingsEditProfileViewController.swift
@@ -0,0 +1,76 @@
+//
+//  SettingsEditProfileViewController.swift
+//  Book
+//
+//  Created by Asad Khaliq on 12/3/15.
+//  Copyright © 2015 Asad Khaliq. All rights reserved.
+//
+
+import UIKit
+
+class SettingsEditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    
+    let imagePicker = UIImagePickerController()
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func changeProfileImageButton(sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func takeProfileImageButton(sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo: [String: AnyObject]) {
+        if let pickedImage = didFinishPickingMediaWithInfo[UIImagePickerControllerOriginalImage] as? UIImage {
+            profileImage.contentMode = .ScaleAspectFit
+            profileImage.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
